ed32640ffaf884e39e2c1b007c5f8556e0ae2b46
Matusalen Alves <matusalencosta@icloud.com>
2026-07-08 14:44:35 -0300

    User delete

--- src/main/java/com/matusalenalves/course/resources/UserResources.java
+++ src/main/java/com/matusalenalves/course/resources/UserResources.java
@@ -34,4 +34,10 @@ public class UserResources {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(user);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
