d008e361c4b98a686b76c1d7f0ad04650e035fd7
Matusalen Alves <matusalencosta@icloud.com>
2026-07-08 23:41:18 -0300

    Config project to deploy

--- src/main/java/com/matusalenalves/course/config/TestConfig.java
+++ src/main/java/com/matusalenalves/course/config/TestConfig.java
@@ -12,7 +12,7 @@ import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
